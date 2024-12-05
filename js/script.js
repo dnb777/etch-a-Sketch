a6b5712667f5305aba1cecb2020c35c787ad42e2
Damian Bruno <damianbruno17@gmail.com>
2024-12-05 15:16:53 -0300

    feat: agrega los estilos de los cuadrados dinamicamente

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const gridContainer = document.querySelector("#grid");
 
-function createGrid(n){
-    for (let i = 0; i < (n * n); i++){
+function createGrid(squaresPerRow){
+    total = squaresPerRow * squaresPerRow;
+    
+    for (let i = 0; i < total; i++){
         let div = document.createElement("div");
-        div.classList = "square";
+        div.style.cssText = 
+        `flex: 0 0 calc(100% / ${squaresPerRow}); 
+        border: 1px solid black;
+        aspect-ratio: 1;`
         gridContainer.appendChild(div);
     }
 }
