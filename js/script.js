7e83f96032cf48a72eb2aa5e4999891473a5ee45
Damian Bruno <damianbruno17@gmail.com>
2024-12-05 16:01:11 -0300

    feat: agrega un eventListener a cada div -cuando el evento mouseenter se dispara -se agrega la clase .paint al div

--- js/script.js
+++ js/script.js
@@ -4,13 +4,21 @@ function createGrid(squaresPerRow){
     total = squaresPerRow * squaresPerRow;
     
     for (let i = 0; i < total; i++){
-        let div = document.createElement("div");
+        const div = document.createElement("div");
+
         div.style.cssText = 
         `flex: 0 0 calc(100% / ${squaresPerRow}); 
         border: 1px solid black;
-        aspect-ratio: 1;`
+        aspect-ratio: 1`;
+
         gridContainer.appendChild(div);
+        
+        div.addEventListener("mouseenter", (e) => {
+            e.target.classList.add("paint");
+        })
     }
 }
 
-createGrid(16);
+createGrid(32);
+
+
